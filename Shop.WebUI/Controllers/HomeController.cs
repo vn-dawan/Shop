bccaac69573781ce459315053c61041601d631b8
vn-dawan <74711005+vn-dawan@users.noreply.github.com>
2020-11-20 16:38:52 +0100

    Filtering by category

--- Shop.WebUI/Controllers/HomeController.cs
+++ Shop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Shop.Core.Models;
+using Shop.Core.ViewModels;
+using Shop.DataAccess.SQL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,43 @@ namespace Shop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        SQLRepository<Product> context;
+        SQLRepository<ProductCategory> contextCategory;
+
+        public HomeController()
         {
-            return View();
+            context = new SQLRepository<Product>(new MyContext());
+            contextCategory = new SQLRepository<ProductCategory>(new MyContext());
+        }
+        public ActionResult Index(string Category=null)
+        {
+            List<Product> products = context.Collection().ToList();
+            List<ProductCategory> categories = contextCategory.Collection().ToList();
+            ProductListViewModel productListView = new ProductListViewModel();
+            if (Category==null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+            productListView.Products = products;
+            productListView.ProductCategories = categories;
+            return View(productListView);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Product product = context.FindById(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
